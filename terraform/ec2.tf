445c9d62285246f645991f0cbe7bcdcbd42216bf
anusree-ux <anusreemanojmk2004@gmail.com>
2026-07-03 12:33:59 +0000

    Provision AWS infrastructure using Terraform

---
+++ terraform/ec2.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "web" {
+
+  ami           = "ami-00cc80a408efefcc0"
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.public.id
+
+  key_name = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.ec2_sg.id
+  ]
+
+  iam_instance_profile = aws_iam_instance_profile.profile.name
+
+  tags = {
+
+    Name = "iac-ec2"
+  }
+}
---
+++ terraform/iam.tf
@@ -0,0 +1,30 @@
+resource "aws_iam_role" "ec2_role" {
+
+  name = "iac-ec2-role"
+
+  assume_role_policy = jsonencode({
+
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+
+        Effect = "Allow"
+
+        Principal = {
+
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "profile" {
+
+  name = "iac-instance-profile"
+
+  role = aws_iam_role.ec2_role.name
+}
